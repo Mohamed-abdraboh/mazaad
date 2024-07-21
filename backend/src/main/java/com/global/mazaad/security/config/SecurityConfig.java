9907312c55ae4ff14239ae8f2d801a3bdc42df7a
Mohamed Abdraboh <mohamedabdraboh015@gmail.com>
2024-07-21 20:01:37 +0300

    add swagger config

--- backend/src/main/java/com/global/mazaad/security/config/SecurityConfig.java
+++ backend/src/main/java/com/global/mazaad/security/config/SecurityConfig.java
@@ -1,6 +1,13 @@
 package com.global.mazaad.security.config;
 
 import com.global.mazaad.security.jwt.JwtAuthenticationFilter;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -102,5 +109,18 @@ public class SecurityConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  private SecurityScheme createAPIKeyScheme() {
+    return new SecurityScheme().type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("bearer");
+  }
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+        .components(
+            new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+        .info(new Info().title("Mazaad API").version("1.0"));
   }
 }
