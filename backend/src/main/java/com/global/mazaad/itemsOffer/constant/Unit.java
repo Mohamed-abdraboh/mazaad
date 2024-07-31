de250472509f664467e18d12f28b7efe35888db5
Mohamed Abdraboh <mohamedabdraboh015@gmail.com>
2024-07-31 12:51:55 +0300

    refactor enum Unit

--- backend/src/main/java/com/global/mazaad/itemsOffer/constant/Unit.java
+++ backend/src/main/java/com/global/mazaad/itemsOffer/constant/Unit.java
@@ -1,35 +1,20 @@
 package com.global.mazaad.itemsOffer.constant;
 
 public enum Unit {
-  GRAM("g"),
-  KILOGRAM("kg"),
-  TON("ton"),
-  LITER("l"),
-  MILLILITER("ml"),
-  PIECE("pcs"),
-  METER("m"),
-  CENTIMETER("cm"),
-  SQUARE_METER("m²"),
-  CUBIC_METER("m³"),
-  GALLON("gal"),
-  YARD("yd"),
-  FOOT("ft"),
-  INCH("in"),
-  POUND("lb"),
-  OUNCE("oz");
-
-  private final String symbol;
-
-  Unit(String symbol) {
-    this.symbol = symbol;
-  }
-
-  public String getSymbol() {
-    return symbol;
-  }
-
-  @Override
-  public String toString() {
-    return symbol;
-  }
+  GRAM,
+  KILOGRAM,
+  TON,
+  LITER,
+  MILLILITER,
+  PIECE,
+  METER,
+  CENTIMETER,
+  SQUARE_METER,
+  CUBIC_METER,
+  GALLON,
+  YARD,
+  FOOT,
+  INCH,
+  POUND,
+  OUNCE;
 }
